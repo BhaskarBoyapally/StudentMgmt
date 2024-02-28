1cd5d71885415f7b1a635300aebf8350d678c2d0
BhaskarBoyapally <94041336+BhaskarBoyapally@users.noreply.github.com>
2024-02-29 05:16:19 +0530

    Update Main.java
    Removed unwanted comments

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.SQLException;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) throws IOException, SQLException {
         System.out.println("Hello and welcome to student management app");
